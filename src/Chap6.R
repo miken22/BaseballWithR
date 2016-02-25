c4bfecbdf0774f382fc84bb28b08761183d48041
Mike Nowicki <mikenowicki022@gmail.com>
2016-02-24 21:29:46 -0800

    Pulled more data, working on chapter 7

--- src/Chap6.R
+++ src/Chap6.R
@@ -10,8 +10,16 @@ require(pacman)
 require(plyr)
 require(ggplot2)
 require(retrosheet)
+require(RSQLite)
 require(pitchRx)
 require(Lahman)
 library(lattice)
 
-sampleRows <- sample(1:nrow(verlander), 20)
+# Will do the first section analying Hutchisons bizarre 2015 season.
+db <- src_sqlite("data/pitchfx.sqlite", create = FALSE)
+#scrape(start = "2015-04-05", end = "2015-11-05", connect = db$con)
+#pitch <- dbGetQuery(db$con, "select pitch_type, px, pz, start_speed, des, gameday_link, num from pitch")
+#bat <- dbGetQuery(db$con, "select batter_name, pitcher_name, event, stand, b_height, gameday_link, num, b, s, o from atbat")
+#combined <- join(bat, pitch, c("gameday_link","num"))
+
+#drew.pitch <- with(combined, subset(combined, pitcher_name == "Drew Hutchison"))
--- src/Chap7.R
+++ src/Chap7.R
@@ -36,5 +36,165 @@ pbp2015 <- read.csv("data/all_data.csv")
 headers <- read.csv("data/fields.csv")
 names(pbp2015) <- headers$Header
 
-tor.games.all <- getRetrosheet("play", 2015, "TOR")
-head(tor.games.all[[5]])
+drew <- subset(Master, Master$nameFirst == "Drew" & Master$nameLast == "Hutchison")
+
+pbp2015 <- read.csv("data/all_data.csv")
+headers <- read.csv("data/fields.csv")
+names(pbp2015) <- headers$Header
+# Retrosheet data is missing games before April 12 for the Jays for some reason, considers
+# Drew's first start as the game vs Baltimore.
+drew.pbp2015 <- subset(pbp2015, pbp2015$PIT_ID == drew$retroID)
+
+# Remove non-pitch events:
+drew.pbp2015$PITCH_SEQ <- gsub("[.>123+*]", "", drew.pbp2015$PITCH_SEQ)
+# Find all pitch sequences (taken from Appendix A)
+drew.pbp2015$c00 <- 1
+drew.pbp2015$c10 <- grepl("^[BIPV]", drew.pbp2015$PITCH_SEQ)
+drew.pbp2015$c01 <- grepl("^[CFKLMOQRST]", drew.pbp2015$PITCH_SEQ)
+drew.pbp2015$c20 <- grepl("^[BIPV]{2}", drew.pbp2015$PITCH_SEQ)
+drew.pbp2015$c30 <- grepl("^[BIPV]{3}", drew.pbp2015$PITCH_SEQ)
+drew.pbp2015$c02 <- grepl("^[CFKLMOQRST]{2}", drew.pbp2015$PITCH_SEQ)
+
+drew.pbp2015$c11 <- grepl("^([CFKLMOQRST][BIPV]|[BIPV][CFKLMOQRST])", drew.pbp2015$PITCH_SEQ)
+drew.pbp2015$c21 <- grepl("^([BIPV][BIPV][CFKLMOQRST]
+                          |[BIPV][CFKLMOQRST][BIPV]
+                          |[CFKLMOQRST][BIPV][BIPV])", drew.pbp2015$PITCH_SEQ)
+drew.pbp2015$c31 <- grepl("^([BIPV][BIPV][BIPV][CFKLMOQRST]
+                          |[BIPV][CFKLMOQRST][BIPV][BIPV]|[BIPV][BIPV][CFKLMOQRST][BIPV]
+                          |[BIPV][BIPV][BIPV][CFKLMOQRST])", drew.pbp2015$PITCH_SEQ)
+
+drew.pbp2015$c12 <- grepl("^([CFKLMOQRST][CFKLMOQRST][FR]*[BIPV]
+                          |[BIPV][CFKLMOQRST][CFKLMOQRST]
+                          |[CFKLMOQRST][BIPV][CFKLMOQRST])", drew.pbp2015$PITCH_SEQ)
+
+drew.pbp2015$c22 <- grepl("^([CFKLMOQRST][CFKLMOQRST][FR]*[BIPV][FR]*[BIPV]
+                          |[BIPV][BIPV][CFKLMOQRST][CFKLMOQRST]
+                          |[BIPV][CFKLMOQRST][BIPV][CFKLMOQRST]
+                          |[BIPV][CFKLMOQRST][CFKLMOQRST][FR]*[BIPV]
+                          |[CFKLMOQRST][BIPV][CFKLMOQRST][FR]*[BIPV]
+                          |[CFKLMOQRST][BIPV][BIPV][CFKLMOQRST])", drew.pbp2015$PITCH_SEQ)
+
+drew.pbp2015$c32 <- grepl("^[CFKLMOQRST]*[BIPV][CFKLMOQRST]*[BIPV][CFKLMOQRST]*[BIPV]", drew.pbp2015$PITCH_SEQ) &
+  grepl("^[BIPV]*[CFKLMOQRST][BIPV]*[CFKLMOQRST]", drew.pbp2015$PITCH_SEQ)
+
+# Convert TRUE/FALSE to 1/0:
+drew.pbp2015$c01 <- gsub("FALSE", 0, drew.pbp2015$c01)
+drew.pbp2015$c01 <- gsub("TRUE", 1, drew.pbp2015$c01)
+drew.pbp2015$c02 <- gsub("FALSE", 0, drew.pbp2015$c02)
+drew.pbp2015$c02 <- gsub("TRUE", 1, drew.pbp2015$c02)
+
+drew.pbp2015$c10 <- gsub("FALSE", 0, drew.pbp2015$c10)
+drew.pbp2015$c10 <- gsub("TRUE", 1, drew.pbp2015$c10)
+drew.pbp2015$c20 <- gsub("FALSE", 0, drew.pbp2015$c20)
+drew.pbp2015$c20 <- gsub("TRUE", 1, drew.pbp2015$c20)
+drew.pbp2015$c30 <- gsub("FALSE", 0, drew.pbp2015$c30)
+drew.pbp2015$c30 <- gsub("TRUE", 1, drew.pbp2015$c30)
+
+drew.pbp2015$c11 <- gsub("FALSE", 0, drew.pbp2015$c11)
+drew.pbp2015$c11 <- gsub("TRUE", 1, drew.pbp2015$c11)
+
+drew.pbp2015$c12 <- gsub("FALSE", 0, drew.pbp2015$c12)
+drew.pbp2015$c12 <- gsub("TRUE", 1, drew.pbp2015$c12)
+
+drew.pbp2015$c21 <- gsub("FALSE", 0, drew.pbp2015$c21)
+drew.pbp2015$c21 <- gsub("TRUE", 1, drew.pbp2015$c21)
+drew.pbp2015$c31 <- gsub("FALSE", 0, drew.pbp2015$c31)
+drew.pbp2015$c31 <- gsub("TRUE", 1, drew.pbp2015$c31)
+
+drew.pbp2015$c22 <- gsub("FALSE", 0, drew.pbp2015$c22)
+drew.pbp2015$c22 <- gsub("TRUE", 1, drew.pbp2015$c22)
+
+drew.pbp2015$c32 <- gsub("FALSE", 0, drew.pbp2015$c32)
+drew.pbp2015$c32 <- gsub("TRUE", 1, drew.pbp2015$c32)
+
+# Compute the run values since I'm not using their provided data:
+drew.pbp2015$RUNS <- with(drew.pbp2015, AWAY_SCORE_CT + HOME_SCORE_CT)
+
+drew.pbp2015$HALF_INNING <- with(drew.pbp2015, paste(GAME_ID, INN_CT, BATTING_TM))
+
+drew.pbp2015$RUNS_SCORED <- with(drew.pbp2015, (BAT_DEST_ID > 3) + (RUN1_DEST_ID > 3) + (RUN2_DEST_ID > 3) + (RUN3_DEST_ID > 3))
+RUNS_SCORED_INNING <- aggregate(drew.pbp2015$RUNS_SCORED, list(HALF_INNING=drew.pbp2015$HALF_INNING), sum)
+RUNS_SCORED_START <- aggregate(drew.pbp2015$RUNS, list(HALF_INNING=drew.pbp2015$HALF_INNING), "[", 1)
+
+MAX <- data.frame(HALF_INNING = RUNS_SCORED_START$HALF_INNING)
+MAX$x <- RUNS_SCORED_INNING$x + RUNS_SCORED_START$x
+
+drew.pbp2015 <- merge(drew.pbp2015, MAX)
+N <- ncol(drew.pbp2015)
+names(drew.pbp2015)[N] <- "MAX_RUNS"
+
+drew.pbp2015$RUNS_ROI <- with(drew.pbp2015, MAX_RUNS - RUNS)
+RUNNER1 <- ifelse (as.character(drew.pbp2015[,"BASE1_RUN_ID"]) == "", 0, 1)
+RUNNER2 <- ifelse (as.character(drew.pbp2015[,"BASE2_RUN_ID"]) == "", 0, 1)
+RUNNER3 <- ifelse (as.character(drew.pbp2015[,"BASE3_RUN_ID"]) == "", 0, 1)
+
+get.state <- function(runner1, runner2, runner3, outs) {
+  runners <- paste(runner1, runner2, runner3, sep="")
+  paste(runners, outs)
+}
+drew.pbp2015$STATE <- get.state(RUNNER1, RUNNER2, RUNNER3, drew.pbp2015$OUTS_CT)
+
+NRUNNER1 <- with(drew.pbp2015, as.numeric(RUN1_DEST_ID == 1 | BAT_DEST_ID == 1))
+NRUNNER2 <- with(drew.pbp2015, as.numeric(RUN1_DEST_ID == 2 | RUN2_DEST_ID == 2 | BAT_DEST_ID == 2))
+NRUNNER3 <- with(drew.pbp2015, as.numeric(RUN1_DEST_ID == 3 | RUN2_DEST_ID == 3 | RUN3_DEST_ID == 3 | BAT_DEST_ID == 3))
+NOUTS <- with(drew.pbp2015, OUTS_CT + EVENT_OUTS_CT)
+drew.pbp2015$NEW_STATE <- get.state(NRUNNER1, NRUNNER2, NRUNNER3, NOUTS)
+
+drew.pbp2015 <- subset(drew.pbp2015, (STATE != NEW_STATE) | RUNS_SCORED > 0)
+
+# Adjust for partial half innings
+data.outs <- ddply(drew.pbp2015, .(HALF_INNING), summarize, Outs.Inning=sum(EVENT_OUTS_CT))
+drew.pbp2015 <- merge(drew.pbp2015, data.outs)
+pbpdata2015C <- subset(drew.pbp2015, Outs.Inning == 3)
+
+RUNS <- with(pbpdata2015C, aggregate(RUNS_ROI, list(STATE), mean))
+
+# Data for Drew in 2015 shows these two situations never arose, so we must manually include them
+RUNS <- rbind(RUNS, c("011 0", as.numeric(0)))
+RUNS <- rbind(RUNS, c("001 0", as.numeric(0)))      
+RUNS$Outs <- substr(RUNS$Group, 5, 5)
+RUNS$x <- as.numeric(RUNS$x)
+RUNS <- RUNS[order(RUNS$Outs),]
+# Creates the run expectancy matrix
+RUNS.out <- matrix(round(RUNS$x, 2), 8, 3)
+dimnames(RUNS.out)[[2]] <- c("0 Outs", "1 Out", "2 Outs")
+dimnames(RUNS.out)[[1]] <- c("000", "001", "010", "011", "100", "101", "110", "111")
+
+# Compute the value of a batting play
+RUNS_POTENTIAL <- matrix(c(RUNS$x, rep(0, 8)), 32, 1)
+dimnames(RUNS_POTENTIAL)[[1]] <- c(RUNS$Group.1, "000 3", "001 3", "010 3", "011 3", "100 3", "101 3", "110 3", "111 3")
+drew.pbp2015$RUNS_STATE <- RUNS_POTENTIAL[drew.pbp2015$STATE, ]
+drew.pbp2015$RUNS_NEW_STATE <- RUNS_POTENTIAL[drew.pbp2015$NEW_STATE, ]
+drew.pbp2015$RUNS_VALUE <- drew.pbp2015$RUNS_NEW_STATE - drew.pbp2015$RUNS_STATE + drew.pbp2015$RUNS_SCORED
+
+ab10 <- subset(drew.pbp2015, c10 == 1)
+ab01 <- subset(drew.pbp2015, c01 == 1)
+
+ab11 <- subset(drew.pbp2015, c01 == 1)
+ab20 <- subset(drew.pbp2015, c01 == 1)
+ab02 <- subset(drew.pbp2015, c01 == 1)
+
+ab21 <- subset(drew.pbp2015, c01 == 1)
+ab12 <- subset(drew.pbp2015, c01 == 1)
+ab30 <- subset(drew.pbp2015, c01 == 1)
+
+ab31 <- subset(drew.pbp2015, c01 == 1)
+ab32 <- subset(drew.pbp2015, c01 == 1)
+
+# Comparing what runs were worth when Drew worked ahead the expected runs are -0.035 in the 0-1 count,
+# and is +0.089 runs when Drew falls behind 1-0.
+c(mean(ab10$RUNS_VALUE), mean(ab01$RUNS_VALUE))
+
+runs.by.count <- expand.grid(balls=0:3, strikes=0:2)
+runs.by.count$values <- 0
+
+bs.count.run.value <- function (b, s) {
+  column.name <- paste("c", b, s, sep="")
+  column.name
+  mean(drew.pbp2015[drew.pbp2015[, column.name] == 1, "RUNS_VALUE"])
+}
+
+runs.by.count$values <- mapply(FUN=bs.count.run.value, 
+                              b = runs.by.count$balls, 
+                              s = runs.by.count$strikes)
+countmap(runs.by.count)
