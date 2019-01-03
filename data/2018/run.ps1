cceb6c16e3734c0aa26713f48ea6ff9f8a12937b
Mike Nowicki <mikenowicki022@gmail.com>
2019-01-03 15:55:41 -0800

    Remove extra data file

--- data/2018/run.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Running the BEVENT TOOL"
-
-Get-ChildItem "L:\BaseballWithR\data\2018" -Filter *.EVA | ForEach-Object {
-    $content = Get-Content $_.FullName
-    Write-Host $content
-}
